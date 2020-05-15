4e3e3322a7f4f9d09c1baf82dda61c18956fb700
lstorm-ux <63728542+lstorm-ux@users.noreply.github.com>
2020-05-15 10:44:39 +0200

    mood checker

---
+++ the-mood-checker.py
@@ -0,0 +1,17 @@
+mood = input("What is your mood today? ")
+
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print("Cheer up, little man.")
+
+elif mood == "excited":
+    print("“It is good to get excited about something.")
+
+else:
+    print("I don't recognize this mood")
